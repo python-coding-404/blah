2ad69b3481bbc3f72c011d23749f23f67ed4c318
python-programing <79223364+python-coding-404@users.noreply.github.com>
2021-09-26 13:20:27 +0000

    Chan

---
+++ README.md
@@ -0,0 +1,4 @@
+# Haikus for Codespaces
+
+This is a quick node project template for demoing Codespaces. It is based off of the [Azure node sample]
+(https://github.com). It's great!!!
--- index.js
+++ index.js
@@ -1,21 +1,44 @@
 const express = require('express');
+
+require('events').setMaxListeners = 1000;
+const { v4: uuidv4 } = require('uuid');
 const app = express();
-const mary = { id: "289-787892"}
+const uuid = uuidv4();
+console.log(uuid)
+const mary = { id: 12781, name: "mary"}
+
+
+
 const bodyParser = require("body-parser")
 app.use(bodyParser.urlencoded({ extended: false }))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname + "/static/"))
-
+app.get(`/${uuid}`, (req, res) => {
+res.send("lol")
+})
 app.get('/xml', (req, res) => {
   res.sendFile(__dirname + "/lol.xml")
 })
+
 app.get("/", (req, res) => {
   res.render("index")
 })
+app.get("/admin", (req, res) => {
+  res.render("admin")
+})
+
 app.post("/home", (req, res) => {
   
-  res.render("home",{ID: req.body.ID})
+  res.render("home",{ID: req.body.ID, Name:req.body.name, res:res})
 })
+app.get("/api/users", (req,res) => {
+  res.json(mary);
+})
+app.get("/user/profile", (req,res) => {
+  
+  res.render("profile", {Name: req.query.Name, ID: req.query.ID})
+})
+
 app.listen(3000, () => {
 });
